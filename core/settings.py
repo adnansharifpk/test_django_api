545f3f627b9dc1375bb96dcc9d095234655738ae
AdnanS <adnansharifpk@gmail.com>
2025-01-26 18:21:18 +0500

    Access to all host allowed

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^4tn)@j6pril8douwa94gl*!%0^d$7uauk#$68mkyxmd5e!$+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 #APPEND_SLASH = True
 # Application definition
